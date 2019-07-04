e41af76766c6c4a2080e60c58816dfd224eacbf6
Evan Daley <daley.evan@gmail.com>
2019-07-04 16:56:21 -0700

    Starting over to quickly resolve pluralization problem

--- app/controllers/pages_controller.rb
+++
@@ -1,6 +0,0 @@
-class PagesController < ApplicationController
-    def about
-        @title = 'About Us'
-        @content = 'This is the About Page'
-    end
-end
--- config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  root 'post#index'
-
-  get 'about' => 'pages#about'
-end
